beb1da4125063ed58a72aa669dd3fcd4c1261a62
Mahmoud876-eng <mahmoudmalek2004@gmail.com>
2025-09-21 16:15:30 +0100

    9/21 0.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
   
     { path: 'dispute', loadComponent: () => import('./crude/dispute/dispute.component').then(m => m.DisputeComponent), canActivate: [AuthGuard] },
     { path: 'table', loadComponent: () => import('./crude/table/table.component').then(m => m.TableComponent) , canActivate: [AuthGuard]},
-    { path: 'client', loadComponent: () => import('./client/client.component').then(m => m.ClientComponent) },
+    { path: 'client', loadComponent: () => import('./client/client.component').then(m => m.ClientComponent), canActivate: [AuthGuard] },
     { path: 'admin', loadComponent: () => import('./admin/admin.component').then(m => m.AdminComponent), canActivate: [AuthGuard] },
     { path: 'dashboard', loadComponent: () => import('./crude/dashboard/dashboard.component').then(m => m.DashboardComponent), canActivate: [AuthGuard] },
     { path: 'user/:id', loadComponent: () => import('./user4/user4.component').then(m => m.User4Component), canActivate: [AuthGuard] },
